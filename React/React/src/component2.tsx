07f40796c76a57a9b71c8f200c542c2e6ace9ea9
Yannick <yannickvreede@gmail.com>
2022-03-09 12:11:54 +0100

    werkt niet

---
+++ React/React/src/component2.tsx
@@ -0,0 +1,73 @@
+/** @format */
+
+import * as React from "react";
+import "./comp.css";
+
+export interface post{
+    id: Number,
+    userid: Number,
+    title: string,
+    body: string,
+}
+
+interface IProps {
+}
+
+interface IState {
+	posts: post[];
+}
+
+export let blogsFC: React.FunctionComponent<post> = (post: post) => {
+	return (
+		<div>
+			<h1>{post.title}</h1>
+            <p>{post.body}</p>
+		</div>
+	);
+};
+
+export default class Comp2 extends React.Component<IProps, IState> {
+	constructor(props: IProps) {
+		super(props);
+
+		this.state = {
+			posts: [],
+		};
+	}
+componentWillMount(): void {
+    this.getData();
+}
+
+getData(): void {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            let temp: post[] = [];
+
+            data.results.map((e: post) =>
+                temp.push({
+                    id : e.id,
+                    userid : e.userid,
+                    title : e.title,
+                    body : e.body,
+                })
+            );
+
+            this.setState({ posts: temp });
+        })
+        .catch((error) => console.log(error + "🚫"));
+}
+render(): React.ReactNode {
+    console.log(this.state);
+
+    return (
+        <div className="container">
+            <br></br>
+
+            {this.state.posts.map((e) => {
+                return blogsFC(e);
+            })}
+        </div>
+    );
+}
+}
--- React/React/src/main.tsx
+++ React/React/src/main.tsx
@@ -5,12 +5,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Comp from "./component";
+import Comp2 from "./component2";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Routes>
 			<Route path={"/"} element={<Comp test={10} />} />
+			<Route path= {"/posts"} element = {<Comp2 />} />
 		</Routes>
 	</BrowserRouter>,
 	document.getElementById("root")
